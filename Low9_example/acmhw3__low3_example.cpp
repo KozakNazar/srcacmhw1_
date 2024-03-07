361d2899f27a4bbc4c7ff43bc35b60ff817b1b17
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2024-03-07 05:39:08 +0200

    Add files via upload

---
+++ Low9_example/acmhw3__low3_example.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <regex>
+
+/* Example for find Low9-lexeme expression:
+(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z)(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z)(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z)
+*/
+std::regex token_re("\\b[a-z]{9}\\b");
+//std::regex token_re("\\b[a-z][a-z][a-z][a-z][a-z][a-z][a-z][a-z][a-z]\\b");
+//std::regex token_re("\\b[a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z][a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z][a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z][a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z][a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z][a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z][a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z][a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z][a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z]\\b");
+//std::regex token_re("\\b[a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z][a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z][a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z][a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z][a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z][a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z][a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z][a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z][a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z]\\b");
+int main()
+{
+	std::string text =
+		"Sir, in my heart there was a kind of fighting "
+		"That would not let me sleep. Methought I lay "
+		"Worse than the mutines in the bilboes. Rashly— "
+		"And prais'd be rashness for it—let us know "
+		"Our indiscretion sometimes serves us well ... "
+		; // — Hamlet, Act 5, Scene 2, 4–8
+
+	std::copy(std::sregex_token_iterator(text.begin(), text.end(), token_re, 0),
+		std::sregex_token_iterator(),
+		std::ostream_iterator<std::string>(std::cout, "\n"));
+
+	return 0;
+}
